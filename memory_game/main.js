43f8577e7943dbbfdbae709eaaba53e0007032a6
Burdette <hb6651@hburdette02LM.local>
2019-10-22 15:50:10 -0400

    created game logic

--- memory_game/main.js
+++ memory_game/main.js
@@ -3,5 +3,27 @@ var cardTwo = "king";
 var cardThree = "queen";
 var cardFour = "king";
 
-console.log("User flipped " + cardOne);
-console.log("User flipped " + cardTwo);
+
+const cards = ["queen","queen","king","king"];
+
+const cardsInPlay = [];
+
+var cardOne = cards[0];
+
+cardsInPlay.push(cardOne);
+	console.log("User flipped queen");
+
+var cardTwo = cards[2];
+
+cardsInPlay.push(cardTwo);
+	console.log("User flipped king");
+
+
+if (cardsInPlay.length === 2) {
+	alert("number of cards in play = 2");
+
+	if (cardsInPlay[0] === cardsInPlay[1])
+		alert("You've found a match!");
+	else 
+		alert("Sorry, try again");
+}
